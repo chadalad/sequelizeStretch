7b0bceefb59ed009f90c6f797d69701d68ef5f51
chadalad <cnuckols7@gmail.com>
2020-06-22 18:55:04 -0400

    recipe

--- recipe.js
+++ recipe.js
@@ -1,7 +1,17 @@
 const db = require('./dbConfig');
 const Sequelize = require('sequelize');
+const { STRING } = Sequelize;
 
 //YOUR CODE GOES HERE
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres:/localhost/sequelize_stretch', {
+  logging: false
+});
+const Recipe = conn.define('recipe', {
+  name: {
+    type: STRING,
+    defaultValue: 'cereal'
+  }
+})
 
 //--------------------
 module.exports = Recipe;
